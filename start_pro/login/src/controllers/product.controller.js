89d28f3e304883d2f706223a64c1be5fe355fdcf
Rahul Baghel <rahulbaghel6211@gmail.com>
2022-03-30 23:31:42 +0530

    auth

---
+++ start_pro/login/src/controllers/product.controller.js
@@ -0,0 +1,15 @@
+// const express=require("express")
+// const router=express.Router();
+// const Product=require("../models/product.models")
+// router.post("",async(req,res)=>{
+
+//     try {
+//         const product=await Product.create(req.body)
+//         return res.status(200).send(product)
+        
+//     } catch (error) {
+//         return res.status(200).send({message:error})
+//     }
+
+// })
+// module.exports=router
---
+++ start_pro/login/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connect=require("./configs/db")
+
+
+ const usersController = require("./controllers/users.controllers");
+const {register,login}=require("./controllers/auth.controller")
+// const productController = require("./controllers/product.controller")
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersController); 
+
+app.post("/register",register)
+app.post("/login",login)
+
+// app.use("/products",productController)
+
+
+
+
+module.exports = app;
