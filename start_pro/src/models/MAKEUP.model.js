f88465357bdbb7f73f37593b361b707b464119ef
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-04-01 10:22:40 +0530

    updated

--- start_pro/src/models/MAKEUP.model.js
+++ start_pro/src/models/MAKEUP.model.js
@@ -1,12 +1,13 @@
 const mongoose=require("mongoose")
 
-const myglammcartSchema =new mongoose.Schema(
+const makeupSchema =new mongoose.Schema(
     {
         Name:{type:String,required:true},
         Price:{type:Number,required:true},
         Description:{type:String,required:true},
         Use:{type:String,required:true},
         benifit:{type:String,required:true},
+        exproperty:{type:String,required:true},
         Image1:{type:String,required:true},
         Image2:{type:String,required:true},
         Image3:{type:String,required:true},
@@ -14,7 +15,8 @@ const myglammcartSchema =new mongoose.Schema(
         Image5:{type:String,required:true},
     },{
         timestamps:true,
+        versionKey:false
     }
 )
 
-module.exports=mongoose.model("myglammcart",myglammcartSchema)
+module.exports=mongoose.model("makeup",makeupSchema)
