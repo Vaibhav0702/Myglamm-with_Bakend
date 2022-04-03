7961620329df4a4ae317d87638ddcc245a6c7ce9
Vaibhav Girish Ginnalwar <97450767+Vaibhav0702@users.noreply.github.com>
2022-04-03 15:39:39 +0530

    updated

--- frontend/scripts/cart.js
+++ frontend/scripts/cart.js
@@ -244,7 +244,7 @@ checkoutbtn.onclick = function()
 
   localStorage.setItem("myglamm-total",JSON.stringify (myglammtotal));
   
-    window.location.href = "/frontend/checkout.html"
+    window.location.href = "../checkout.html"
 }
 
 
